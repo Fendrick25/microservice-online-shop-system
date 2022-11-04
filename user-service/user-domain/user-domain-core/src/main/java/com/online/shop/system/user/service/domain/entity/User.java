3d782936ec233e7090312fe696451514b249733c
FEN <fendrick85@gmail.com>
2022-11-04 13:51:19 +0700

    Added user-service

--- user-service/user-domain/user-domain-core/src/main/java/com/online/shop/system/user/service/domain/entity/User.java
+++ user-service/user-domain/user-domain-core/src/main/java/com/online/shop/system/user/service/domain/entity/User.java
@@ -1,9 +1,13 @@
 package com.online.shop.system.user.service.domain.entity;
 
+import com.online.shop.system.user.service.domain.valueobject.Address;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -11,5 +15,16 @@ import java.util.UUID;
 @AllArgsConstructor
 public class User {
 
-    private final UUID id;
+    private UUID id;
+    private final String username;
+    private final String name;
+    private final String email;
+    private final String password;
+    private List<Address> address;
+    private ZonedDateTime createdAt;
+
+    public void initializeUser(){
+        id = UUID.randomUUID();
+        createdAt = ZonedDateTime.now(ZoneId.of("UTC"));
+    }
 }
